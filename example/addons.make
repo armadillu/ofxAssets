24738609dfa3cfd74fbd52627147df49ce75c755
Oriol Ferrer Mesia <oriol@uri.cat>
2017-12-03 11:39:29 -0500

    add ci

---
+++ example/addons.make
@@ -0,0 +1,4 @@
+ofxPoco
+ofxSimpleHttp
+ofxTagSystem
+ofxThreadSafeLog
---
+++ scripts/ci/vs/install.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+set -e
+
+echo "Executing ci/vs/install.sh"
+
+export OF_ROOT="$APPVEYOR_BUILD_FOLDER/../openFrameworks"
+export OF_ADDONS=$OF_ROOT/addons
+
+pwd;
+echo "OF_ADDONS: $OF_ADDONS"
+
+ADDONS="armadillu/ofxSimpleHttp armadillu/ofxTagSystem armadillu/ofxThreadSafeLog"
+
+cd $OF_ADDONS
+
+for ADDON in $ADDONS
+do
+  echo "Cloning addon '$ADDON' to " `pwd`
+  git clone --depth=1  https://github.com/$ADDON.git
+done
+
+cd -
